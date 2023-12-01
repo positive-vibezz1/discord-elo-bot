83084a08ece5f639c64e9937f25c44278d28a646
positive_vibezz <134086715+positive-vibezz1@users.noreply.github.com>
2023-12-01 15:45:58 -0500

    vrml_elo_bot.py

--- vrml_elo_bot.py
+++ vrml_elo_bot.py
@@ -61,40 +61,6 @@ async def win(ctx):
 
         print(f"New Rating for teams: {new_rating_teams}")
         
-        await ctx.send(f"New Rating for teams: {new_rating_teams}")
-
-'''
-@bot.command(name='lost')
-async def lost(ctx):
-     if ctx.channel.id == 1180168660778745978:
-        response = 0
-        await ctx.send("Updating Elo...")
-    
-        # Example usage:
-        team_home = "Silly_Willy"
-        team_away = "Lunch_Room_Bandits"
-
-        rating_home = team_ratings[team_home]
-        rating_away = team_ratings[team_away]
+        await ctx.send(f"New Rating for teams: {new_rating_teams}")  
         
-        won = response
-        weight = 20
-        game_weight = Update_rating(rating_home, rating_away, won, weight)
-
-        updating_elo = Update_rating(rating_home, rating_away, won, weight)
-
-        game_outcome_weight = game_weight.expected_outcome()
-
-        new_rating_teams = updating_elo.update_elo_rating()
-        
-        team_ratings[team_home] = new_rating_teams[0]
-        team_ratings[team_away] = new_rating_teams[1]
-
-        print(f"expected outcome is: {game_outcome_weight}")
-
-        print(f"New Rating for teams: {new_rating_teams}")
-        
-        await ctx.send(f"New Rating for teams: {new_rating_teams}")
-'''        
-        
-bot.run('MTE4MDE2MjgwNzk3MTQ0Njc5NQ.G-l2wm.HlsSNYCFGTlxHIkq4Ye7XTzw_zi-l0JyAZP_q8')
+bot.run('bot token')
